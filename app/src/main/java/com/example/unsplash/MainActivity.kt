925c313120bbb05104555dcbf0509e0eeb79365e
Anastasia <agnibeda99@gmail.com>
2021-09-23 20:12:56 +0300

    hw2

--- app/src/main/java/com/example/unsplash/MainActivity.kt
+++ app/src/main/java/com/example/unsplash/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        Timber.plant(Timber.DebugTree())
+    }
 }
